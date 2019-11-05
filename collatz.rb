70a3971617a70689a081e779e0cbcd642ce8d242
Conner Krall <conner.krall@gmail.com>
2019-11-05 04:35:10 +0000

    final solution

--- collatz.rb
+++ collatz.rb
@@ -1,30 +1,35 @@
-attr_accessor
 
 def collatz(n)
-  a = []
+  a = [n]
   until n == 1
-    if n % 2 == 0
+    if n % 2 == 0  #if even divide by 2
       n = n/2
-      a.push(n)
     else
-      n = 3*n + 1
-      a.push(n) 
+      n = 3*n + 1  #if odd use formula
+    end
+    a << n        #push the number into the array
+  end
+  a
+end
+
+puts collatz(5)
+
+def longest_collatz
+  count = 1
+  longest = []          #establish array to hold longest sequence
+  until count == 1000000
+    seq = collatz(count)  
+    if seq.size > longest.size #if current seq is longer than longest
+      longest = seq
+      count += 1
+    else
+      count += 1
     end
   end
+  puts "The number #{longest[0]} has the longest sequence at #{longest.size}"
 end
 
-def seq(a)
-end
+puts longest_collatz
 
-def longest(num)
 
-end
 
-puts seq(2)
-puts collatz(1)
-puts collatz(2)
-puts collatz(3)
-puts collatz(4)
-puts collatz(5)
-puts collatz(6)
-puts collatz(7)
